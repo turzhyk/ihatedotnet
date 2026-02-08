f35fdb2a60f2e88e7f8e602e61f61710ef1ee819
Turzhyk <andrewurbro@yahoo.com>
2026-02-08 12:41:43 +0100

    st 2

---
+++ OrderStore.Core/Models/OrderItem.cs
@@ -0,0 +1,18 @@
+using System.Text.Json.Nodes;
+
+namespace OrderStore.Core.Models;
+
+public enum OrderItemType
+{
+    Businesscard,
+    Banner
+}
+public class OrderItem
+{
+    public Guid Id { get; set; }
+    public int Quantity { get; set; }
+    public OrderItemType Type { get; set; }
+    public decimal PricePerUnit { get; set; }
+    public decimal TotalPrice { get; set; }
+    public string Opinion { get; set; }
+}
